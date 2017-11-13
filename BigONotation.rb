48427cf5ec921f2ebc6e8e112ea37eba7276ccfd
Eric Yang <ericliuyang@gmail.com>
2017-11-13 12:47:37 -0800

    Create BigONotation.rb

---
+++ BigONotation.rb
@@ -0,0 +1,6 @@
+Big O Notation from Hackerrank
+Four Rules: 
+1. Different steps get added.
+2. Drop constants   Ex: O(2n) ===> O(n)
+3. Different Inputs => Different variables
+4. Drop non-determinate terms. Ex: O(n + n^2) ===> O(n^2)
